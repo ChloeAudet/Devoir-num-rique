e6d50bb7a9ba45ff1b6700fbf7efc28b15f7f4eb
corinnedumais <dumais.corinne@gmail.com>
2020-04-16 15:59:40 -0400

    no3 avance

---
+++ Numero3/GraphiqueSR.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Noms : Corinne Dumais, Élodie Lescure, Chloé Lavoie-Audet, Aricia Proulx
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+Nh = 301  # nombre de noeuds horizontaux
+Nv = 51  # nombre de noeuds verticaux
+
+# initialisation de la matrice
+cavite = np.array([[0 for x in range(Nh)] for y in range(Nv)])
+
+# initialisation du potentiel sur la paroi supérieur
+for n in range(20, Nh):
+    cavite[0][n] = 300
+
+# initialisation du potentiel sur la paroi inférieur
+for n in range(20, Nh):
+    cavite[50][n] = 300
+
+# initialisation du potentiel sur la paroi de droite
+for n in range(Nv):
+    cavite[n][300] = 300
+
+# initialisation du potentiel sur la paroi de gauche
+for n in range(9):
+    cavite[n][20] = 300
+
+for n in range(42, Nv):
+    cavite[n][20] = 300
+
+for n in range(15, 36):
+    cavite[n][0] = 300
+
+for n in range(8, 16):
+    cavite[n][10] = 300
+
+for n in range(35, 43):
+    cavite[n][10] = 300
+
+for n in range(11):
+    cavite[15][n] = 300
+
+for n in range(10, 21):
+    cavite[8][n] = 300
+
+for n in range(11):
+    cavite[35][n] = 300
+
+for n in range(10, 21):
+    cavite[42][n] = 300
+
+# assignation d'une valeur au nombre d'itérations
+nb_iterations = 40
+n = 0
+w = 0.7
+
+list_iterations = []
+list_diff = []
+
+# calcul Gauss-Seidel avec sur-relaxation
+while n <= nb_iterations:
+    somme_diff = 0
+    for j in range(1, Nv - 1):
+        for i in range(1, Nh - 1):
+            if 22 < j < 28 and i > 14:
+                continue
+            elif j < 9 and i < 21:
+                continue
+            elif i < 11 and j < 16:
+                continue
+            elif i < 21 and j > 41:
+                continue
+            elif j > 34 and i < 11:
+                continue
+            else:
+                ancienne_valeur = cavite[j][i]
+                cavite[j][i] = 0.25 * (1 + w) * (cavite[j + 1][i] + cavite[j - 1][i]
+                                                 + cavite[j][i + 1] + cavite[j][i - 1]) - w * cavite[j][i]
+                somme_diff += (cavite[j][i] - ancienne_valeur)
+    n += 1
+    list_iterations.append(n)
+    list_diff.append(somme_diff/14191)
+
+# affichage du graphique pour la méthode de la relaxation
+plt.plot(list_iterations, list_diff)
+plt.xlabel("Nombre d'itérations")
+plt.ylabel("Variation moyenne[V]")
+plt.show()
